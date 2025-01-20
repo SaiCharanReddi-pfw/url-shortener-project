24e408b640ab4c3c2f5f0bb673b6688f9f9e92eb
sandeepvennam <sandeepvennam11@gmail.com>
2025-01-20 22:01:23 +0000

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -7,14 +7,3 @@ terraform {
     encrypt        = true
   }
 }
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
