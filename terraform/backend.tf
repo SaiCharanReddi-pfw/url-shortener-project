cebccbb21903d53146e8a0acd9bd747b43ebfcab
sandeepvennam <sandeepvennam11@gmail.com>
2025-01-20 17:36:09 +0000

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,18 +1,15 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-url-shortener-state"
-    key            = "url-shortener/terraform.tfstate"
-    region         = "eu-west-2"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-url-shortener-state"
+  versioning {
+    enabled = true
   }
 }
 
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
+# Create DynamoDB table for state locking
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform-locks"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
   attribute {
     name = "LockID"
     type = "S"
