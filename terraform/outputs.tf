d8b63e4e79676c39da6406ddf99e092d4f0d00f8
sandeepvennam <sandeepvennam11@gmail.com>
2025-01-20 20:50:09 +0000

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,3 @@
 output "api_url" {
-  value = aws_api_gateway_rest_api.url_shortener_api.execution_arn
+  value = "https://${aws_api_gateway_rest_api.url_shortener_api.id}.execute-api.${var.region}.amazonaws.com/${aws_api_gateway_stage.api_stage.stage_name}"
 }
-
